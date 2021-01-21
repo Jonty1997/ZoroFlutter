bcf78f54619e517fa5ad0d0b4fa589fb1ed448f2
tiki.lqt <tiki.lqt>
2021-01-21 20:51:19 +0800

    修改名称并统一导出 common 的包

---
+++ lib/common/common.dart
@@ -0,0 +1,22 @@
+///统一常用导包文件
+export 'package:flutter/material.dart';
+export 'package:flutter/foundation.dart';
+export 'package:provider/provider.dart';
+
+///========================config
+export 'package:zoro_flutter/common/config/route.dart';
+export 'package:zoro_flutter/common/config/multi_provider_config.dart';
+
+
+///========================mixin
+export 'package:zoro_flutter/common/mixin/after_layout_mixin.dart';
+
+///========================provider
+export 'package:zoro_flutter/common/provider/theme_provider.dart';
+
+///========================res
+export 'package:zoro_flutter/common/res/colors.dart';
+
+///========================util
+export 'package:zoro_flutter/common/util/common_util.dart';
+export 'package:zoro_flutter/common/util/datebase_util.dart';
