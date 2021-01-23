13ac26763e033738ba8f55dfe80ddf0a51c7d39b
tiki.lqt <tiki.lqt>
2021-01-23 18:00:27 +0800

    添加状态栏管理

--- lib/common/provider/theme_provider.dart
+++ lib/common/provider/theme_provider.dart
@@ -4,10 +4,16 @@ import 'package:zoro_flutter/common/common.dart';
 class ThemeProvider with ChangeNotifier, DiagnosticableTreeMixin {
   Brightness _brightness = Brightness.light;
   Color _primarySwatch = Colors.green;
+  Color _statusBarColor = Colors.transparent; //状态栏颜色
+  Brightness _statusBarIconBrightness = Brightness.dark; //状态栏字体颜色
 
   Brightness get brightness => _brightness;
 
+  Brightness get statusBarIconBrightness => _statusBarIconBrightness;
+
   Color get primarySwatch => _primarySwatch;
+
+  Color get statusBarColor => _statusBarColor;
 
   void setBrightness(Brightness brightness) {
     _brightness = brightness;
@@ -16,6 +22,16 @@ class ThemeProvider with ChangeNotifier, DiagnosticableTreeMixin {
 
   void setPrimarySwatch(Color color) {
     _primarySwatch = color;
+    notifyListeners();
+  }
+
+  void setStatusBarColor(Color color) {
+    _statusBarColor = color;
+    notifyListeners();
+  }
+
+  void setStatusBarIconBrightness(Brightness brightness) {
+    _statusBarIconBrightness = brightness;
     notifyListeners();
   }
 }
