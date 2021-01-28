f14a4df200ab5598d090826da5628f2841497d0b
tiki.lqt <tiki.lqt>
2021-01-28 18:14:41 +0800

    修改container 的高度

--- lib/widget/widgets/stateless_widget/container_demo.dart
+++ lib/widget/widgets/stateless_widget/container_demo.dart
@@ -43,7 +43,7 @@ class _ContainerDemoState extends State<ContainerDemo> {
           ),
           Container(
             width: 200,
-            height: 500,
+            height: 200,
             alignment: _alignment,
             color: Colors.purple,
             child: Container(
