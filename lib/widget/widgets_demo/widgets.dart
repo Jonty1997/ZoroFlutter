4834b983f1f7e4ee12c184348b17d9ebb9902d45
tiki.lqt <tiki.lqt>
2021-01-28 19:27:58 +0800

    导包

---
+++ lib/widget/widgets_demo/widgets.dart
@@ -0,0 +1,5 @@
+///widget 统一导包出去
+
+///======================== stateless_widget
+export "../widgets_demo/stateless_widget/container_demo.dart";
+export "../widgets_demo/stateless_widget/about_dialog_demo.dart";
