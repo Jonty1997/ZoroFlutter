7787a9cdf73696e0b3a445a23dad1b47a5c76bc0
tiki.lqt <tiki.lqt>
2021-01-28 11:44:46 +0800

    添加通用圆角

--- lib/common/res/styles.dart
+++ lib/common/res/styles.dart
@@ -1,7 +1,13 @@
 import 'package:zoro_flutter/common/common.dart';
 
 ///一些通用的格式
-///
-///
+
+///通用边距
 var commonPadding =
     EdgeInsets.symmetric(horizontal: Width.w_16, vertical: Height.h_10);
+
+///通用的圆角
+var commonRadius = BorderRadius.all(Radius.circular(Width.w_16));
+
+
+
