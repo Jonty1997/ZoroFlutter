d9a177bec057a1546c16fb7ef3c28f27f2b4ba70
tiki.lqt <tiki.lqt>
2021-01-28 14:19:39 +0800

    添加更多同样样式

--- lib/common/res/height.dart
+++ lib/common/res/height.dart
@@ -2,5 +2,6 @@
 ///[h_10]为全局的上下布局间距
 ///
 class Height {
+  static final double h_5 = 5;
   static final double h_10 = 10;
 }
--- lib/common/res/styles.dart
+++ lib/common/res/styles.dart
@@ -6,8 +6,12 @@ import 'package:zoro_flutter/common/common.dart';
 var commonPadding =
     EdgeInsets.symmetric(horizontal: Width.w_16, vertical: Height.h_10);
 
+///通用小边距
+var commonLittlePadding =
+    EdgeInsets.symmetric(horizontal: Width.w_10, vertical: Height.h_5);
+
 ///通用的圆角
 var commonRadius = BorderRadius.all(Radius.circular(Width.w_16));
 
-
-
+///通用小圆角
+var commonLittleRadius = BorderRadius.all(Radius.circular(Width.w_10));
